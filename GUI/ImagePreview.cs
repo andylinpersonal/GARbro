cd6088e6e0a6dabaf5974f631a8a2e2554cc2800
andylinpersonal <andylinpersonal@gmail.com>
2019-08-12 01:39:06 +0800

    make ImageCanvas zoomable...

--- GUI/ImagePreview.cs
+++ GUI/ImagePreview.cs
@@ -46,14 +46,14 @@ namespace GARbro.GUI
 {
     public partial class MainWindow : Window
     {
-        private readonly BackgroundWorker   m_preview_worker = new BackgroundWorker();
-        private PreviewFile                 m_current_preview = new PreviewFile();
-        private bool                        m_preview_pending = false;
+        private readonly BackgroundWorker m_preview_worker = new BackgroundWorker();
+        private PreviewFile m_current_preview = new PreviewFile();
+        private bool m_preview_pending = false;
 
         private UIElement m_active_viewer;
         public UIElement ActiveViewer
         {
-            get { return m_active_viewer;  }
+            get { return m_active_viewer; }
             set
             {
                 if (value == m_active_viewer)
@@ -69,7 +69,7 @@ namespace GARbro.GUI
                         exists = true;
                 }
                 if (!exists)
-                    PreviewPane.Children.Add (m_active_viewer);
+                    PreviewPane.Children.Add(m_active_viewer);
             }
         }
 
@@ -79,15 +79,15 @@ namespace GARbro.GUI
             public string Name { get; set; }
             public Entry Entry { get; set; }
 
-            public bool IsEqual (IEnumerable<string> path, string name)
+            public bool IsEqual(IEnumerable<string> path, string name)
             {
-                return Path != null && path.SequenceEqual (Path) && name.Equals (Name);
+                return Path != null && path.SequenceEqual(Path) && name.Equals(Name);
             }
         }
 
-        private void InitPreviewPane ()
+        private void InitPreviewPane()
         {
-            m_preview_worker.DoWork += (s, e) => LoadPreviewImage (e.Argument as PreviewFile);
+            m_preview_worker.DoWork += (s, e) => LoadPreviewImage(e.Argument as PreviewFile);
             m_preview_worker.RunWorkerCompleted += (s, e) => {
                 if (m_preview_pending)
                     RefreshPreviewPane();
@@ -99,34 +99,34 @@ namespace GARbro.GUI
         private IEnumerable<Encoding> m_encoding_list = GetEncodingList();
         public IEnumerable<Encoding> TextEncodings { get { return m_encoding_list; } }
 
-        internal static IEnumerable<Encoding> GetEncodingList (bool exclude_utf16 = false)
+        internal static IEnumerable<Encoding> GetEncodingList(bool exclude_utf16 = false)
         {
             var list = new HashSet<Encoding>();
-            try 
+            try
             {
                 list.Add(Encoding.Default);
                 var oem = CultureInfo.CurrentCulture.TextInfo.OEMCodePage;
                 list.Add(Encoding.GetEncoding(oem));
-            } 
-            catch (Exception X) 
+            }
+            catch (Exception X)
             {
-                if (X is ArgumentException || X is NotSupportedException) 
+                if (X is ArgumentException || X is NotSupportedException)
                     list.Add(Encoding.GetEncoding(20127)); //default to US-ASCII
-                else 
+                else
                     throw;
             }
-            list.Add (Encoding.GetEncoding (932));
-            list.Add (Encoding.GetEncoding (936));
-            list.Add (Encoding.UTF8);
+            list.Add(Encoding.GetEncoding(932));
+            list.Add(Encoding.GetEncoding(936));
+            list.Add(Encoding.UTF8);
             if (!exclude_utf16)
             {
-                list.Add (Encoding.Unicode);
-                list.Add (Encoding.BigEndianUnicode);
+                list.Add(Encoding.Unicode);
+                list.Add(Encoding.BigEndianUnicode);
             }
             return list;
         }
- 
-        private void OnEncodingSelect (object sender, SelectionChangedEventArgs e)
+
+        private void OnEncodingSelect(object sender, SelectionChangedEventArgs e)
         {
             var enc = this.EncodingChoice.SelectedItem as Encoding;
             if (null == enc || null == CurrentTextInput)
@@ -137,24 +137,24 @@ namespace GARbro.GUI
         /// <summary>
         /// Display entry in preview panel
         /// </summary>
-        private void PreviewEntry (Entry entry)
+        private void PreviewEntry(Entry entry)
         {
-            if (m_current_preview.IsEqual (ViewModel.Path, entry.Name))
+            if (m_current_preview.IsEqual(ViewModel.Path, entry.Name))
                 return;
-            UpdatePreviewPane (entry);
+            UpdatePreviewPane(entry);
         }
 
-        void RefreshPreviewPane ()
+        void RefreshPreviewPane()
         {
             m_preview_pending = false;
             var current = CurrentDirectory.SelectedItem as EntryViewModel;
             if (null != current)
-                UpdatePreviewPane (current.Source);
+                UpdatePreviewPane(current.Source);
             else
                 ResetPreviewPane();
         }
 
-        void ResetPreviewPane ()
+        void ResetPreviewPane()
         {
             ActiveViewer = ImageView;
             ImageCanvas.Source = null;
@@ -162,26 +162,26 @@ namespace GARbro.GUI
             CurrentTextInput = null;
         }
 
-        bool IsPreviewPossible (Entry entry)
+        bool IsPreviewPossible(Entry entry)
         {
             return "image" == entry.Type || "script" == entry.Type
-                || (string.IsNullOrEmpty (entry.Type) && entry.Size < 0x100000);
+                || (string.IsNullOrEmpty(entry.Type) && entry.Size < 0x100000);
         }
 
-        void UpdatePreviewPane (Entry entry)
+        void UpdatePreviewPane(Entry entry)
         {
-            SetStatusText ("");
+            SetStatusText("");
             var vm = ViewModel;
             m_current_preview = new PreviewFile { Path = vm.Path, Name = entry.Name, Entry = entry };
-            if (!IsPreviewPossible (entry))
+            if (!IsPreviewPossible(entry))
             {
                 ResetPreviewPane();
                 return;
             }
             if ("image" != entry.Type)
-                LoadPreviewText (m_current_preview);
+                LoadPreviewText(m_current_preview);
             else if (!m_preview_worker.IsBusy)
-                m_preview_worker.RunWorkerAsync (m_current_preview);
+                m_preview_worker.RunWorkerAsync(m_current_preview);
             else
                 m_preview_pending = true;
         }
@@ -200,13 +200,13 @@ namespace GARbro.GUI
             }
         }
 
-        void LoadPreviewText (PreviewFile preview)
+        void LoadPreviewText(PreviewFile preview)
         {
             Stream file = null;
             try
             {
-                file = VFS.OpenBinaryStream (preview.Entry).AsStream;
-                if (!TextView.IsTextFile (file))
+                file = VFS.OpenBinaryStream(preview.Entry).AsStream;
+                if (!TextView.IsTextFile(file))
                 {
                     ResetPreviewPane();
                     return;
@@ -214,10 +214,10 @@ namespace GARbro.GUI
                 var enc = EncodingChoice.SelectedItem as Encoding;
                 if (null == enc)
                 {
-                    enc = TextView.GuessEncoding (file);
+                    enc = TextView.GuessEncoding(file);
                     EncodingChoice.SelectedItem = enc;
                 }
-                TextView.DisplayStream (file, enc);
+                TextView.DisplayStream(file, enc);
                 ActiveViewer = TextView;
                 CurrentTextInput = file;
                 file = null;
@@ -225,7 +225,7 @@ namespace GARbro.GUI
             catch (Exception X)
             {
                 ResetPreviewPane();
-                SetStatusText (X.Message);
+                SetStatusText(X.Message);
             }
             finally
             {
@@ -234,43 +234,48 @@ namespace GARbro.GUI
             }
         }
 
-        void LoadPreviewImage (PreviewFile preview)
+        void LoadPreviewImage(PreviewFile preview)
         {
             try
             {
-                using (var data = VFS.OpenImage (preview.Entry))
+                using (var data = VFS.OpenImage(preview.Entry))
                 {
-                    SetPreviewImage (preview, data.Image.Bitmap);
+                    SetPreviewImage(preview, data.Image.Bitmap);
                 }
             }
             catch (Exception X)
             {
-                Dispatcher.Invoke (ResetPreviewPane);
-                SetStatusText (X.Message);
+                Dispatcher.Invoke(ResetPreviewPane);
+                SetStatusText(X.Message);
             }
         }
 
-        void SetPreviewImage (PreviewFile preview, BitmapSource bitmap)
+        void SetPreviewImage(PreviewFile preview, BitmapSource bitmap)
         {
             if (bitmap.DpiX != Desktop.DpiX || bitmap.DpiY != Desktop.DpiY)
             {
-                int stride = bitmap.PixelWidth * ((bitmap.Format.BitsPerPixel + 7) / 8); 
-                var pixels = new byte[stride*bitmap.PixelHeight];
-                bitmap.CopyPixels (pixels, stride, 0);
-                var fixed_bitmap = BitmapSource.Create (bitmap.PixelWidth, bitmap.PixelHeight,
+                int stride = bitmap.PixelWidth * ((bitmap.Format.BitsPerPixel + 7) / 8);
+                var pixels = new byte[stride * bitmap.PixelHeight];
+                bitmap.CopyPixels(pixels, stride, 0);
+                var fixed_bitmap = BitmapSource.Create(bitmap.PixelWidth, bitmap.PixelHeight,
                     Desktop.DpiX, Desktop.DpiY, bitmap.Format, bitmap.Palette, pixels, stride);
                 bitmap = fixed_bitmap;
             }
             if (!bitmap.IsFrozen)
                 bitmap.Freeze();
-            Dispatcher.Invoke (() =>
+            Dispatcher.Invoke(() =>
             {
                 if (m_current_preview == preview) // compare by reference
                 {
                     ActiveViewer = ImageView;
+                    #region ADL
+                    // Reset scrolled canvas
+                    ImageCanvas.Height = double.NaN;
+                    ImageCanvas.Width = double.NaN;
+                    #endregion
                     ImageCanvas.Source = bitmap;
                     ApplyDownScaleSetting();
-                    SetStatusText (string.Format (guiStrings.MsgImageSize, bitmap.PixelWidth,
+                    SetStatusText(string.Format(guiStrings.MsgImageSize, bitmap.PixelWidth,
                                                   bitmap.PixelHeight, bitmap.Format.BitsPerPixel));
                 }
             });
@@ -279,21 +284,21 @@ namespace GARbro.GUI
         /// <summary>
         /// Fit window size to image.
         /// </summary>
-        private void FitWindowExec (object sender, ExecutedRoutedEventArgs e)
+        private void FitWindowExec(object sender, ExecutedRoutedEventArgs e)
         {
             var image = ImageCanvas.Source;
             if (null == image)
                 return;
             var width = image.Width + Settings.Default.lvPanelWidth.Value + 1;
             var height = image.Height;
-            width = Math.Max (ContentGrid.ActualWidth, width);
-            height = Math.Max (ContentGrid.ActualHeight, height);
+            width = Math.Max(ContentGrid.ActualWidth, width);
+            height = Math.Max(ContentGrid.ActualHeight, height);
             if (width > ContentGrid.ActualWidth || height > ContentGrid.ActualHeight)
             {
                 ContentGrid.Width = width;
                 ContentGrid.Height = height;
                 this.SizeToContent = SizeToContent.WidthAndHeight;
-                Dispatcher.InvokeAsync (() => {
+                Dispatcher.InvokeAsync(() => {
                     this.SizeToContent = SizeToContent.Manual;
                     ContentGrid.Width = double.NaN;
                     ContentGrid.Height = double.NaN;
@@ -301,25 +306,25 @@ namespace GARbro.GUI
             }
         }
 
-        private void SetImageScaleMode (bool scale)
+        private void SetImageScaleMode(bool scale)
         {
             if (scale)
             {
                 ImageCanvas.Stretch = Stretch.Uniform;
-                RenderOptions.SetBitmapScalingMode (ImageCanvas, BitmapScalingMode.HighQuality);
+                RenderOptions.SetBitmapScalingMode(ImageCanvas, BitmapScalingMode.HighQuality);
                 ImageView.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled;
                 ImageView.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
             }
             else
             {
                 ImageCanvas.Stretch = Stretch.None;
-                RenderOptions.SetBitmapScalingMode (ImageCanvas, BitmapScalingMode.NearestNeighbor);
+                RenderOptions.SetBitmapScalingMode(ImageCanvas, BitmapScalingMode.NearestNeighbor);
                 ImageView.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
                 ImageView.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
             }
         }
 
-        private void ApplyDownScaleSetting ()
+        private void ApplyDownScaleSetting()
         {
             bool image_need_scale = DownScaleImage.Get<bool>();
             if (image_need_scale && ImageCanvas.Source != null)
@@ -327,15 +332,55 @@ namespace GARbro.GUI
                 var image = ImageCanvas.Source;
                 image_need_scale = image.Width > ImageView.ActualWidth || image.Height > ImageView.ActualHeight;
             }
-            SetImageScaleMode (image_need_scale);
+            SetImageScaleMode(image_need_scale);
         }
 
-        private void PreviewSizeChanged (object sender, SizeChangedEventArgs e)
+        private void PreviewSizeChanged(object sender, SizeChangedEventArgs e)
         {
             var image = ImageCanvas.Source;
             if (null == image || !DownScaleImage.Get<bool>())
                 return;
-            SetImageScaleMode (image.Width > e.NewSize.Width || image.Height > e.NewSize.Height);
+            SetImageScaleMode(image.Width > e.NewSize.Width || image.Height > e.NewSize.Height);
         }
+        #region ADL
+        
+        private void ImageCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                var image = ImageCanvas.Source;
+                if (image == null) return;
+                double ratio = image.Width / image.Height;
+                if (double.IsNaN(ImageCanvas.Height) || double.IsNaN(ImageCanvas.Width))
+                {
+                    ImageCanvas.Height = ImageCanvas.ActualHeight;
+                    ImageCanvas.Width = ImageCanvas.ActualWidth;
+                    ImageView.CanContentScroll = true;
+                }
+
+                ImageCanvas.Height *= (1 + e.Delta / 1200d);
+                ImageCanvas.Width *= (1 + e.Delta / 1200d);
+
+                if (ImageCanvas.Width > PreviewPane.ActualWidth || ImageCanvas.Height > PreviewPane.ActualHeight)
+                {
+                    ImageView.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                    ImageView.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+                    ImageCanvas.Stretch = Stretch.Uniform;
+                }
+            }
+        }
+
+
+        private void ImageCanvas_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount >= 2)
+            {
+                // Restore size of ImageCanvas...
+                ImageCanvas.Height = Double.NaN;
+                ImageCanvas.Width = Double.NaN;
+                ApplyDownScaleSetting();
+            }
+        }
+        #endregion ADL
     }
 }
